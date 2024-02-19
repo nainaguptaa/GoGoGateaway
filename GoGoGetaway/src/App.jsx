add726b5c7b223871b0904464568de3271648837
dietban19 <dieter.banaag@ucalgary.ca>
2024-02-19 16:04:54 -0700

    more initial

--- GoGoGetaway/src/App.jsx
+++ GoGoGetaway/src/App.jsx
@@ -3,6 +3,8 @@ import { Routes, Route } from 'react-router-dom';
 import Signup from './pages/Auth/Signup';
 import LandingPage from './pages/LandingPage/LandingPage';
 import Profile from './pages/Profile';
+import SearchResults from './pages/SearchResults';
+import ForYou from './pages/ForYou';
 function App() {
   return (
     <>
@@ -10,6 +12,8 @@ function App() {
         <Route path="/" element={<LandingPage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/profile" element={<Profile />} />
+        <Route path="/search" element={<SearchResults />} />
+        <Route path="/foryou" element={<ForYou />} />
       </Routes>
     </>
   );
