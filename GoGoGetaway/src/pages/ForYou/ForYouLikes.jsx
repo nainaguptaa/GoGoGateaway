1493e0f2a6c370f58045011911d185882b7d0a46
dietban19 <dieter.banaag@ucalgary.ca>
2024-02-22 17:49:57 -0700

    added id to itineraries

--- GoGoGetaway/src/pages/ForYou/ForYouLikes.jsx
+++ GoGoGetaway/src/pages/ForYou/ForYouLikes.jsx
@@ -1,33 +1,32 @@
 import React from 'react';
+import {
+  FaRegHeart,
+  FaHeart,
+  FaShare,
+  FaRegCommentAlt,
+  FaCommentAlt,
+} from 'react-icons/fa';
 
-export default function ForYouLikes() {
+import { FaRegBookmark, FaBookmark } from 'react-icons/fa6';
+export default function ForYouLikes({ isMobile, itinerariesDummy, index }) {
   return (
     <div className="h-70 absolute bottom-40 right-6 z-10 mb-12 flex flex-col gap-6 rounded-xl  bg-white/60 px-2 py-4 text-sm sm:mb-8 sm:ml-4 sm:text-lg lg:static lg:right-16 lg:bg-transparent">
       <div className="flex flex-col items-center gap-2">
-        {isMobile ? (
-          <FaRegHeart
-            size={30}
-            className="ease  cursor-pointer text-rose-500 transition duration-200 hover:text-rose-500"
-          />
-        ) : (
-          <FaRegHeart
-            size={30}
-            className="ease  cursor-pointer text-rose-500 transition duration-200 hover:text-rose-500"
-          />
-        )}
+        <FaRegHeart
+          size={30}
+          className="ease  cursor-pointer text-rose-500 transition duration-200 hover:text-rose-500"
+          onClick={() => console.log(itinerariesDummy[index])}
+        />
 
         <div className=" font-bold">{itinerariesDummy[index].likes}</div>
       </div>
       <div className="flex flex-col items-center gap-2">
-        {isMobile ? (
-          <FaRegCommentAlt size={30} />
-        ) : (
-          <FaRegCommentAlt size={30} />
-        )}
+        <FaRegCommentAlt size={30} />
+
         <div className=" font-bold">{itinerariesDummy[index].comments}</div>
       </div>
       <div className="flex flex-col items-center gap-2">
-        {isMobile ? <FaRegBookmark size={30} /> : <FaRegBookmark size={30} />}
+        <FaRegBookmark size={30} />
         {/* <div className="text-lg font-bold">
           {itinerariesDummy[index].likes}
         </div> */}
