2bb11fe578a11e289251ab9bdb3516a1ffdceb74
nainaguptaa <naina.gupta@ucalgary.ca>
2024-03-22 00:20:49 -0600

    made changes to profile page

--- GoGoGetaway/src/pages/Profile/Profile.jsx
+++ GoGoGetaway/src/pages/Profile/Profile.jsx
@@ -1,179 +1,87 @@
-// import React, { useState } from 'react';
-// import { useUserContext } from '../../context/userContext';
-
-
-// const Profile = () => {
-//   const { currentUser, logout } = useUserContext();
-//   const [firstName, setFirstName] = useState(currentUser.firstName);
-//   const [lastName, setLastName] = useState(currentUser.lastName);
-
-//   const handleFirstNameChange = (e) => {
-//     setFirstName(e.target.value);
-//   };
-
-//   const handleLastNameChange = (e) => {
-//     setLastName(e.target.value);
-//   };
-
-//   return (
-//     <div>
-//       <div className="border border-gray-200 rounded-lg p-4 bg-gray-100 h-96 flex flex-col justify-start">
-//         <div className="flex">
-//           <h6 className="font-bold mr-5">Name</h6>
-//           <div className="flex flex-col mr-4">
-//             <label className="mr-2">First Name:</label>
-//             <input
-//               type="text"
-//               className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2"
-//               value={firstName}
-//               onChange={handleFirstNameChange}
-//             />
-//           </div>
-//           <div className="flex flex-col mr-2">
-//             <label>Last Name:</label>
-//             <input
-//               type="text"
-//               className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2"
-//               value={lastName}
-//               onChange={handleLastNameChange}
-//             />
-//           <div>
-//           <button className="border border-gray-700 rounded-lg px-5 py-1 h-7 ml-30">Edit</button>
-//           </div>
-//           </div>
-//           <h6 className="font-bold mt-3">User Name</h6>
-//           <div className="flex">
-//             {currentUser.username}
-//           </div>
-//         </div>
-//         <button onClick={() => logout()} className='border border-red-600 rounded-lg px-3 py-1 h-7 flex justify-center'>Logout</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Profile;
-
-
-// import React, { useState } from 'react';
-// import { useUserContext } from '../../context/userContext';
-
-// const Profile = () => {
-//   const { currentUser, logout } = useUserContext();
-//   const [firstName, setFirstName] = useState(currentUser.firstName);
-//   const [lastName, setLastName] = useState(currentUser.lastName);
-
-//   const handleFirstNameChange = (e) => {
-//     setFirstName(e.target.value);
-//   };
-
-//   const handleLastNameChange = (e) => {
-//     setLastName(e.target.value);
-//   };
-
-//   return (
-//     <div>
-//       <div className="border border-gray-200 rounded-lg p-4 bg-gray-100 h-96 flex flex-col justify-start">
-//         <div className="flex">
-//           <h6 className="font-bold mr-14">Name</h6>
-//           <div className="flex flex-col mr-5">
-//             <label className="mr-2">First Name:</label>
-//             <input
-//               type="text"
-//               className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2"
-//               value={currentUser.firstName}
-//               onChange={handleFirstNameChange}
-//             />
-//           </div>
-//           <div className="flex flex-col mr-2">
-//             <label>Last Name:</label>
-//             <div className="flex items-center"> {/* Wrap both last name input and edit button */}
-//               <input
-//                 type="text"
-//                 className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2"
-//                 value={currentUser.lastName}
-//                 onChange={handleLastNameChange}
-//               />
-//               <button className="border border-gray-700 rounded-lg px-5 py-0 h-7 ml-5 mb-2">Edit</button> {/* Adjust ml-2 for spacing */}
-//               <img src={currentUser.photoURL} />
-//             </div>
-//           </div>
-//         </div>
-//         <h6 className="font-bold mt-3 ">User Name</h6>
-//         <div className="flex ">
-//           {/* {currentUser.username} */}
-//           <input
-//                 type="text"
-//                 className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2 ml-24"
-//                 value={currentUser.username}
-//                 onChange={handleLastNameChange}
-//               />
-//         </div>
-//         <button onClick={() => logout()} className='border border-red-600 rounded-lg px-3 py-1 h-7 flex justify-center'>Logout</button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default Profile;
-
-
-
 import React, { useState } from 'react';
 import { useUserContext } from '../../context/userContext';
 
 const Profile = () => {
   const { currentUser, logout } = useUserContext();
-  const [firstName, setFirstName] = useState(currentUser.firstName);
-  const [lastName, setLastName] = useState(currentUser.lastName);
+  const [birthdate, setBirthdate] = useState(currentUser.birthdate);
 
-  const handleFirstNameChange = (e) => {
-    setFirstName(e.target.value);
+  const handleBirthdateChange = (e) => {
+    const inputValue = e.target.value.replace(/\D/g, ''); // Remove non-numeric characters
+    const formattedValue = formatBirthdate(inputValue);
+    setBirthdate(formattedValue);
   };
 
-  const handleLastNameChange = (e) => {
-    setLastName(e.target.value);
+  const formatBirthdate = (value) => {
+    let formattedValue = value;
+
+    if (formattedValue.length > 2) {
+      formattedValue = formattedValue.substring(0, 2) + '/' + formattedValue.substring(2);
+    }
+    if (formattedValue.length > 5) {
+      formattedValue = formattedValue.substring(0, 5) + '/' + formattedValue.substring(5, 9);
+    }
+
+    return formattedValue;
   };
 
   return (
     <div>
-      <div className="border border-gray-200 rounded-lg p-4 bg-gray-100 h-96 flex flex-col justify-start">
         <div className="flex">
-          <h6 className="font-bold mr-14">Name</h6>
+          <h6 className="font-bold mr-10 mt-20 ml-4">Name</h6>
           <div className="flex flex-col mr-5">
-            <label className="mr-2">First Name:</label>
+            <label className="mt-20">First Name:</label>
             <input
               type="text"
               className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2"
               value={currentUser.firstName}
-              onChange={handleFirstNameChange}
+              readOnly // Setting readOnly to make it non-editable
             />
           </div>
-          <div className="flex flex-col mr-2">
+          <div className="flex flex-col ml-5 mt-20">
             <label>Last Name:</label>
-            <div className="flex items-center"> {/* Wrap both last name input and edit button */}
+            <div className="flex items-center">
               <input
                 type="text"
                 className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2"
                 value={currentUser.lastName}
-                onChange={handleLastNameChange}
+                readOnly // Setting readOnly to make it non-editable
               />
             </div>
           </div>
         </div>
-        <h6 className="font-bold mt-3 ">User Name</h6>
-        <div className="flex ">
-          {/* {currentUser.username} */}
+        <h6 className="font-bold mt-20 ml-4">Username</h6>
+        <div className="flex">
           <input
-                type="text"
-                className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2 ml-24"
-                value={currentUser.username}
-                onChange={handleLastNameChange}
-              />
+            type="text"
+            className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2 ml-24"
+            value={currentUser.username}
+            readOnly // Setting readOnly to make it non-editable
+          />
         </div>
-        <button onClick={() => logout()} className='border border-red-600 rounded-lg px-3 py-1 h-7 mt-40 flex justify-center text-justify'>Logout</button>
+        <h6 className="font-bold mt-20 ml-4">Email</h6>
+        <div className="flex">
+          <input
+            type="text"
+            className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2 ml-24"
+            value={currentUser.email}
+            readOnly // Setting readOnly to make it non-editable
+          />
+        </div>
+        <h6 className="font-bold mt-20 ml-4">Birthdate</h6> {/* Birthdate section */}
+        <div className="flex">
+          <input
+            type="text"
+            className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2 ml-24 mr-24" // Adjust styling as needed
+            value={birthdate} // Set the value to the birthdate state
+            onChange={handleBirthdateChange} // Handle changes to the birthdate
+            maxLength={10} // Limit the maximum number of characters to 10
+            placeholder="MM/DD/YYYY" // Set placeholder text
+          />
+        </div>
+        <div className="flex justify-center mt-10"> 
+        <button onClick={() => logout()} className='border border-blue-600 rounded-lg px-6 py-2 text-blue-600'>Logout</button>
       </div>
-    </div>
+      </div>
   );
 };
 
