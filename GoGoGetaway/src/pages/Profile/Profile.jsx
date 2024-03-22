2d35633614bd1d84d6b7bc9ec12ca1603e596503
nainaguptaa <naina.gupta@ucalgary.ca>
2024-03-22 00:36:50 -0600

    more changes

--- GoGoGetaway/src/pages/Profile/Profile.jsx
+++ GoGoGetaway/src/pages/Profile/Profile.jsx
@@ -26,62 +26,63 @@ const Profile = () => {
 
   return (
     <div>
-        <div className="flex">
-          <h6 className="font-bold mr-10 mt-20 ml-4">Name</h6>
-          <div className="flex flex-col mr-5">
-            <label className="mt-20">First Name:</label>
+      <h1 className="font-bold text-3xl mt-10 ml-4">Personal Details</h1> 
+      <div className="flex">
+        <h6 className="font-bold mr-10 mt-20 ml-4">Name</h6>
+        <div className="flex flex-col mr-5">
+          <label className="mt-20">First Name:</label>
+          <input
+            type="text"
+            className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2"
+            value={currentUser.firstName}
+            readOnly // Setting readOnly to make it non-editable
+          />
+        </div>
+        <div className="flex flex-col ml-5 mt-20">
+          <label>Last Name:</label>
+          <div className="flex items-center">
             <input
               type="text"
               className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2"
-              value={currentUser.firstName}
+              value={currentUser.lastName}
               readOnly // Setting readOnly to make it non-editable
             />
           </div>
-          <div className="flex flex-col ml-5 mt-20">
-            <label>Last Name:</label>
-            <div className="flex items-center">
-              <input
-                type="text"
-                className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2"
-                value={currentUser.lastName}
-                readOnly // Setting readOnly to make it non-editable
-              />
-            </div>
-          </div>
         </div>
-        <h6 className="font-bold mt-20 ml-4">Username</h6>
-        <div className="flex">
-          <input
-            type="text"
-            className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2 ml-24"
-            value={currentUser.username}
-            readOnly // Setting readOnly to make it non-editable
-          />
-        </div>
-        <h6 className="font-bold mt-20 ml-4">Email</h6>
-        <div className="flex">
-          <input
-            type="text"
-            className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2 ml-24"
-            value={currentUser.email}
-            readOnly // Setting readOnly to make it non-editable
-          />
-        </div>
-        <h6 className="font-bold mt-20 ml-4">Birthdate</h6> {/* Birthdate section */}
-        <div className="flex">
-          <input
-            type="text"
-            className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2 ml-24 mr-24" // Adjust styling as needed
-            value={birthdate} // Set the value to the birthdate state
-            onChange={handleBirthdateChange} // Handle changes to the birthdate
-            maxLength={10} // Limit the maximum number of characters to 10
-            placeholder="MM/DD/YYYY" // Set placeholder text
-          />
-        </div>
-        <div className="flex justify-center mt-10"> 
+      </div>
+      <h6 className="font-bold mt-20 ml-4">Username</h6>
+      <div className="flex">
+        <input
+          type="text"
+          className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2 ml-24"
+          value={currentUser.username}
+          readOnly // Setting readOnly to make it non-editable
+        />
+      </div>
+      <h6 className="font-bold mt-20 ml-4">Email</h6>
+      <div className="flex">
+        <input
+          type="text"
+          className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2 ml-24"
+          value={currentUser.email}
+          readOnly // Setting readOnly to make it non-editable
+        />
+      </div>
+      <h6 className="font-bold mt-20 ml-4">Birthdate</h6> {/* Birthdate section */}
+      <div className="flex">
+        <input
+          type="text"
+          className="border border-gray-700 rounded-lg px-3 py-1 h-7 mb-2 ml-24 mr-24" // Adjust styling as needed
+          value={birthdate} // Set the value to the birthdate state
+          onChange={handleBirthdateChange} // Handle changes to the birthdate
+          maxLength={10} // Limit the maximum number of characters to 10
+          placeholder="MM/DD/YYYY" // Set placeholder text
+        />
+      </div>
+      <div className="flex justify-center mt-3"> 
         <button onClick={() => logout()} className='border border-blue-600 rounded-lg px-60 py-2 text-white bg-teal-500'>Logout</button>
       </div>
-      </div>
+    </div>
   );
 };
 
