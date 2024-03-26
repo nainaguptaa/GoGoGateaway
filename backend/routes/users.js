32c1d6fd368157ab26160cc362a2e41dbfe98af4
dietban19 <dieter.banaag@ucalgary.ca>
2024-03-25 22:18:03 -0600

    fixed styling for itinerary, and save itinerary works

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 // Import the Firebase Admin SDK instance
-const admin = require('firebase-admin');
+const admin = require("firebase-admin");
 
 // Get a reference to the Firestore database
 const db = admin.firestore();
@@ -11,18 +11,56 @@ const db = admin.firestore();
 router.get("/:id", async (req, res) => {
   try {
     const userId = req.params.id;
-    const userRef = db.collection('users').doc(userId);
+    const userRef = db.collection("users").doc(userId);
     const user = await userRef.get();
     if (!user.exists) {
-      return res.status(404).json({ error: 'User not found' });
+      return res.status(404).json({ error: "User not found" });
     }
     return res.json(user.data());
   } catch (error) {
-    console.error('Error fetching user:', error);
-    return res.status(500).json({ error: 'Something went wrong' });
+    console.error("Error fetching user:", error);
+    return res.status(500).json({ error: "Something went wrong" });
   }
 });
 
+router.post("/:userId/save-itinerary", async (req, res) => {
+  const userId = req.params.userId; // Retrieve the user ID from the URL parameters
+  const itineraryId = req.body.itineraryId; // Assuming the itinerary ID is sent in the request body
 
+  if (!itineraryId) {
+    return res.status(400).json({ error: "Missing itinerary ID" });
+  }
+
+  try {
+    // Reference to the user's document in the 'users' collection
+    const userRef = db.collection("users").doc(userId);
+    const doc = await userRef.get();
+
+    if (!doc.exists) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    const userData = doc.data();
+
+    // Check if the itineraryId already exists in the savedItineraries array
+    if (
+      userData.savedItineraries &&
+      userData.savedItineraries.includes(itineraryId)
+    ) {
+      // Itinerary is already saved
+      return res.status(409).json({ message: "Itinerary already saved" });
+    }
+
+    // Atomically add the itinerary ID to the 'savedItineraries' array field in the user's document
+    await userRef.update({
+      savedItineraries: admin.firestore.FieldValue.arrayUnion(itineraryId),
+    });
+
+    res.status(200).json({ message: "Itinerary saved successfully" });
+  } catch (error) {
+    console.error("Error saving itinerary:", error);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
 
 module.exports = router;
